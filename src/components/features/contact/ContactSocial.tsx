3de7fc9058f027584d0cf51a6498a9ca4935d457
soskar1 <s.oskar.21345@gmail.com>
2026-01-22 15:57:04 +0100

    links update

--- src/components/features/contact/ContactSocial.tsx
+++ src/components/features/contact/ContactSocial.tsx
@@ -1,4 +1,4 @@
-import { Github, Linkedin, Mail, UserCircle, X } from "lucide-react";
+import { Github, Linkedin, Mail, UserCircle } from "lucide-react";
 import React from "react";
 import { EMAIL } from "../../../constants";
 import { trackContactFormSubmit } from "../../../hooks/useAnalytics";
@@ -23,7 +23,16 @@ const ContactSocial: React.FC<ContactSocialProps> = ({ onCopyEmail }) => {
       name: "GitHub",
     },
     {
-      icon: X,
+      icon: (props) => (
+        <svg
+          viewBox="-3 -3 30 30"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+          {...props}
+        >
+          <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
+        </svg>
+      ),
       href: "https://x.com/devoskarsledz",
       name: "X (Twitter)",
     },
